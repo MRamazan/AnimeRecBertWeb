7a0f4b76fba057258527103b505059a6dffc378d
MRamazan <mramazantrn28@gmail.com>
2025-07-08 13:33:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from datetime import datetime
 import random
 import re
 from flask import Flask, render_template, request, jsonify, session, Response
+import os
+
+port = int(os.environ.get("PORT", 5000))
 
 app = Flask(__name__)
 app.secret_key = '1903bjk'
@@ -915,7 +918,7 @@ def main():
         print(f"Failed to initialize recommendation system: {e}")
         sys.exit(1)
 
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=port)
 
 
 if __name__ == "__main__":
