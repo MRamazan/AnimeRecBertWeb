ae6ba18f2661d941265b8576213e585937a2e713
MRamazan <mramazantrn28@gmail.com>
2025-07-10 20:05:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -915,7 +915,7 @@ def main():
         print(f"Failed to initialize recommendation system: {e}")
         sys.exit(1)
 
-    app.run(debug=True, host='0.0.0.0', port=5000,allow_unsafe_werkzeug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
 
 if __name__ == "__main__":
